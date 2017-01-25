ed2983b49c3e66af2a06797234031d5526f13250
Gabriela Mininel Valle <gabrieladvmininel@gmail.com>
2017-01-25 22:44:35 +0000

    Modifying Django's serving page to index.html through new songs app creation. Experimentation with buttons and simple html for the search page.

--- song_search/song_search/wsgi.py
+++ song_search/song_search/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for songsearch project.
+WSGI config for song_search project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "songsearch.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "song_search.settings")
 
 application = get_wsgi_application()
