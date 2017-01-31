0afa796dbf9e86683d29df29db9eb4de555a320e
Gabriela Mininel Valle <gabrieladvmininel@gmail.com>
2017-01-31 22:44:59 +0000

    Database first settings: declaring Django Class songs

--- song_search/songs/models.py
+++ song_search/songs/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Songs(models.Model):
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=512, null=True)
+    song_name = models.CharField(max_length=512)
+    lyrics = models.TextField(null=True)
