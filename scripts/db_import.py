677417c944972b346905586096d050005829dcc3
Gabriela Mininel Valle <gabrieladvmininel@gmail.com>
2017-02-01 18:29:35 +0000

    Script and Models modification to import data from table to MySQL

--- scripts/db_import.py
+++ scripts/db_import.py
@@ -1,6 +1,12 @@
 import csv
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="123", db="song_search")
 
 with open('songdata.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    cursor = db.cursor()
     for row in spamreader:
-        print(row[0])
+        cursor.execute("INSERT INTO songs_songs (artist, song_name, lyrics_link, lyrics) VALUES (%s, %s, %s, %s)", (
+            row[0], row[1], row[2], row[3]))
+    db.commit()
