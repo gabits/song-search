168ff0a27fc0bebe078506b0eb6618754f156c8b
Gabriela Mininel Valle <gabrieladvmininel@gmail.com>
2017-02-04 18:30:45 +0000

    Pagination added to results page

--- song_search/songs/views.py
+++ song_search/songs/views.py
@@ -11,7 +11,7 @@ def search_songs(request):
     search_text = (request.GET['search_box']).strip()
     has_search_filter = request.GET.has_key('filter')
     if len(search_text) > 0 and has_search_filter:
-        results_list = []
+
         selected_filter = request.GET['filter']
         if selected_filter == 'artist':
             filt = Songs.objects.filter(artist__icontains=search_text)
@@ -19,7 +19,20 @@ def search_songs(request):
             filt = Songs.objects.filter(song_name__icontains=search_text)
         else:
             filt = Songs.objects.filter(lyrics__icontains=search_text)
-        for row in filt.order_by('artist', 'song_name'):
+
+        filtered_results = filt.order_by('artist', 'song_name')
+
+        pagination = Paginator(filtered_results, 25)
+        num_current_page = request.GET.get('page')
+        try:
+            page = pagination.page(num_current_page)
+        except PageNotAnInteger:
+            page = pagination.page(1)
+        except EmptyPage:
+            page = pagination.page(pagination.num_pages)
+
+        results_list = []
+        for row in page.object_list:
             if len(row.lyrics) <= 100:
                 lyrics_preview = row.lyrics
             else:
@@ -31,10 +44,48 @@ def search_songs(request):
                 'id': row.id,
             }
             results_list.append(song_data)
+
+        # Navigation through pages
+        if page.number - 1 < 1:
+            prev_page = 1
+        else:
+            prev_page = page.previous_page_number()
+        if page.number + 1 > pagination.num_pages:
+            next_page = pagination.num_pages
+        else:
+            next_page = page.next_page_number()
+        if page.number - 10 < 1:
+            previous_10 = 1
+        else:
+            previous_10 = page.number - 10
+        if page.number - 100 < 1:
+            previous_100 = 1
+        else:
+            previous_100 = page.number - 100
+        if page.number + 10 < 1:
+            next_10 = 1
+        else:
+            next_10 = page.number + 10
+        if page.number + 100 < 1:
+            next_100 = 1
+        else:
+            next_100 = page.number + 100
+
         context = {
-            'result': results_list
+            'result': results_list,
+            'page': page,
+            'prev_page': prev_page,
+            'next_page': next_page,
+            'previous_10': previous_10,
+            'previous_100': previous_100,
+            'next_10': next_10,
+            'next_100': next_100,
+            'last_page': pagination.num_pages,
+            'search_box': request.GET['search_box'],
+            'filter': request.GET['filter'],
         }
         return render(request, 'results.html', context)
+
     elif len(search_text) > 0 and not has_search_filter:
         context = {
             'error': 'Choose a filter for your search.',
@@ -58,13 +109,16 @@ def manage(request):
     songs_list = Songs.objects.all().order_by('artist', 'song_name')
     pagination = Paginator(songs_list, 25)
     num_current_page = request.GET.get('page')
+
     try:
         page = pagination.page(num_current_page)
     except PageNotAnInteger:
         page = pagination.page(1)
     except EmptyPage:
         page = pagination.page(pagination.num_pages)
+
     all_page_songs = []
+
     for row in page.object_list:
         if len(row.lyrics) <= 100:
             lyrics_preview = row.lyrics
